13f05b289df15dc23585a330b4404f5635b22837
Grant <grantimaxi03@gmail.com>
2024-04-20 17:53:17 -0400

    Lint fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -8,16 +8,16 @@ request(urlRequest, (error, response, body) => {
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   fs.writeFile(filePath, body, err => {
     if (err) {
-        console.error('Error occurred:', err);
-        return;
+      console.error('Error occurred:', err);
+      return;
     }
     // Successful operation
     try {
-        const stats = fs.statSync(filePath);
-        console.log(`Successfully downloaded and saved ${stats.size} bytes to ${filePath}`);
+      const stats = fs.statSync(filePath);
+      console.log(`Successfully downloaded and saved ${stats.size} bytes to ${filePath}`);
     } catch (error) {
-        console.error('Error retrieving file stats:', error);
+      console.error('Error retrieving file stats:', error);
     }
-});
+  });
 });
 
