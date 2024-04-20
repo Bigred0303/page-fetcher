bbb8121ecdc97aff507754b24a8f5b1803ca87f5
Grant <grantimaxi03@gmail.com>
2024-04-20 17:51:46 -0400

    Creat fetcher file to write html to local file

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+const fs = require('fs');
+const urlRequest = process.argv[2];
+const filePath = process.argv[3];
+
+
+request(urlRequest, (error, response, body) => {
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  fs.writeFile(filePath, body, err => {
+    if (err) {
+        console.error('Error occurred:', err);
+        return;
+    }
+    // Successful operation
+    try {
+        const stats = fs.statSync(filePath);
+        console.log(`Successfully downloaded and saved ${stats.size} bytes to ${filePath}`);
+    } catch (error) {
+        console.error('Error retrieving file stats:', error);
+    }
+});
+});
+
